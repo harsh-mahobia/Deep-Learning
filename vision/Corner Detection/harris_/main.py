a338444b88d979e388ba6419daa3c0594ba01538
Harsh <harsh10mahobia@gmail.com>
2025-10-18 11:36:39 +0530

    harris

---
+++ vision/Corner Detection/harris_/main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def find_corners(name):
+    img = cv2.imread(name)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    #you can play with parameters for the better understanding
+    corners = cv2.goodFeaturesToTrack(gray, 100, 0.20, 10)
+    print("default", corners)
+    corners = np.intp(corners)
+    print("after processing", corners)
+    for i in corners:
+        x,y = i.ravel()
+        cv2.circle(img,(x,y),6,255,2)        
+    plt.imshow(img)
+    plt.show()
+
+
+find_corners('img.png')
+find_corners('chess.png')
